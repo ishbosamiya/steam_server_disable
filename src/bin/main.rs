9a277c2666ff8d4daebd51f78231bb955a3d1c63
ishbosamiya <ishbosamiya@gmail.com>
2021-05-08 12:49:06 +0530

    serverobject: get info from json file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,11 @@
+use steam_server_disable::ServerObject;
+
 fn main() {
-    println!("hello world!");
+    let obj = ServerObject::new();
+    let ip_list = obj.get_server_ips("sgp");
+    println!("ip_list: {:?}", ip_list);
+    let server_list = obj.get_server_list();
+    println!("server_list: {:?}", server_list);
+
+    let ipt = iptables::new(false).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,64 @@
 pub mod downloader;
+
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct ServerObject {
+    json_obj: json::JsonValue,
+}
+
+impl ServerObject {
+    pub fn new() -> Self {
+        let mut downloader = downloader::Download::from_url("https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/Random/NetworkDatagramConfig.json");
+        let file_path = "network_datagram_config.json";
+        downloader.store_to_file(file_path);
+
+        let mut file = File::open(file_path).unwrap();
+        let mut json_data = String::new();
+        file.read_to_string(&mut json_data).unwrap();
+
+        let json_obj = json::parse(&json_data).unwrap();
+
+        Self { json_obj }
+    }
+
+    pub fn get_server_ips(&self, server: &str) -> Vec<&str> {
+        let obj = &self.json_obj;
+
+        let obj = &obj["pops"];
+
+        let server = &obj[server];
+
+        let mut ips = Vec::new();
+
+        if let json::JsonValue::Array(relays) = &server["relays"] {
+            for relay in relays {
+                if let json::JsonValue::Short(ip) = &relay["ipv4"] {
+                    ips.push(ip.as_str());
+                } else {
+                    panic!("couldn't find ip within relays, got {:?}", relay["ipv4"]);
+                }
+            }
+        } else {
+            panic!("couldn't get relays");
+        }
+
+        return ips;
+    }
+
+    pub fn get_server_list(&self) -> Vec<&str> {
+        let obj = &self.json_obj;
+        let obj = &obj["pops"];
+
+        let mut names = Vec::new();
+        if let json::JsonValue::Object(servers) = &obj {
+            for (server, _) in servers.iter() {
+                names.push(server);
+            }
+        } else {
+            panic!("couldn't find array of servers in pops, got {:?}", obj);
+        }
+
+        return names;
     }
 }
