0971d74762406ffb4a8418df6698826812b68c49
ishbosamiya <ishbosamiya@gmail.com>
2021-06-06 19:37:06 +0530

    main: remove deadcode

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,17 +1,6 @@
 use iced::Sandbox;
 use steam_server_disable::ui::UI;
-// use steam_server_disable::ServerObject;
 
 fn main() -> iced::Result {
-    // let obj = ServerObject::new();
-    // let server_list = obj.get_server_list();
-
-    // for server in &server_list {
-    //     let ip_list = obj.get_server_ips(server);
-    //     println!("{}: {:?}", server, ip_list);
-    // }
-
-    // let _ipt = iptables::new(false).unwrap();
-
     UI::run(iced::Settings::default())
 }
